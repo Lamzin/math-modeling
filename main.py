65b02ee06d8469dde5efbd124c633fe08217805d
Oleh Lamzin <olehtomas@gmail.com>
2017-10-21 22:34:35 +0300

    refactoring

--- main.py
+++ main.py
@@ -14,57 +14,51 @@ class Modeling(QTabWidget):
         ui_path = os.path.join(os.path.dirname(__file__), "tabwidget.ui")
         uic.loadUi(ui_path, self)
 
+        self.__init__common__stuff__()
+        self.__init__tab2__stuff__()
+
+    def __init__common__stuff__(self):
         self.points = []
 
-        self.__init_scene__()
+    def __init__tab2__stuff__(self):
+        self.tab2_graphics_scene = QGraphicsScene()
+        self.tab2_graphics.setScene(self.tab2_graphics_scene)
 
-        self.graphicsView.mousePressEvent = self.graphics_view_mouse_click
-        self.graphicsView.mouseMoveEvent = self.graphics_view_mouse_move
+        def graphics_draw():
+            self.tab2_graphics_scene.clear()
+            for x, y in self.points:
+                self.tab2_graphics_scene.addItem(QGraphicsEllipseItem(x - 1, y - 1, 2, 2))
+            for i in range(1, len(self.points)):
+                self.tab2_graphics_scene.addItem(QGraphicsLineItem(
+                    self.points[i - 1][0], self.points[i - 1][1], self.points[i][0], self.points[i][1]))
 
-        self.tableWidget.itemClicked = self.table_item_click
-        self.tableWidget.cellClicked.connect(self.cell_was_clicked)
+        def add_point(x, y):
+            self.tab2_table.setRowCount(len(self.points) + 1)
+            self.tab2_table.setItem(len(self.points), 0, QTableWidgetItem(str(x)))
+            self.tab2_table.setItem(len(self.points), 1, QTableWidgetItem(str(y)))
+            self.points.append((x, y))
+            graphics_draw()
 
-    def cell_was_clicked(self, row, column):
-        if column == 2:
-            self.points = self.points[:row] + self.points[row+1:]
-            self.tableWidget.removeRow(row)
-            self.draw()
+        def graphics_mouse_click(QMouseEvent):
+            self.tab2_graphics_scene.setSceneRect(
+                0, 0, self.tab2_graphics.size().width(), self.tab2_graphics.size().height())
+            add_point(QMouseEvent.pos().x(), QMouseEvent.pos().y())
+        self.tab2_graphics.mousePressEvent = graphics_mouse_click
 
-    def __init_scene__(self):
-        self.scene = QGraphicsScene()
-        self.graphicsView.setScene(self.scene)
+        def graphics_mouse_move(QMouseEvent):
+            graphics_draw()
+            if len(self.points) > 0:
+                line = QGraphicsLineItem(
+                    self.points[-1][0], self.points[-1][1], QMouseEvent.pos().x(), QMouseEvent.pos().y())
+                self.tab2_graphics_scene.addItem(line)
+        self.tab2_graphics.mouseMoveEvent = graphics_mouse_move
 
-    def graphics_view_mouse_click(self, QMouseEvent):
-        self.scene.setSceneRect(0, 0, self.graphicsView.size().width(), self.graphicsView.size().height())
-        self.add_point(QMouseEvent.pos().x(), QMouseEvent.pos().y())
-
-    def graphics_view_mouse_move(self, QMouseEvent):
-        self.draw()
-        if len(self.points) > 0:
-            line = QGraphicsLineItem(
-                self.points[-1][0], self.points[-1][1], QMouseEvent.pos().x(), QMouseEvent.pos().y())
-            self.scene.addItem(line)
-
-    def table_item_click(self, QListWidgetItem):
-        print(QListWidgetItem.row())
-
-    def add_point(self, x, y):
-        # w = QTableWidget()
-        self.tableWidget.setRowCount(len(self.points) + 1)
-        self.tableWidget.setItem(len(self.points), 0, QTableWidgetItem(str(x)))
-        self.tableWidget.setItem(len(self.points), 1, QTableWidgetItem(str(y)))
-        self.points.append((x, y))
-        self.draw()
-
-    def draw(self):
-        self.scene.clear()
-        for x, y in self.points:
-            point = QGraphicsEllipseItem(x - 1, y - 1, 2, 2)
-            self.scene.addItem(point)
-        for i in range(1, len(self.points)):
-            line = QGraphicsLineItem(
-                self.points[i - 1][0], self.points[i - 1][1], self.points[i][0], self.points[i][1])
-            self.scene.addItem(line)
+        def table_cell_clicked(row, column):
+            if column == 2:
+                self.points = self.points[:row] + self.points[row + 1:]
+                self.tab2_table.removeRow(row)
+                graphics_draw()
+        self.tab2_table.cellClicked.connect(table_cell_clicked)
 
 
 if __name__ == '__main__':
